971a7927c8b4c970ba65fdf2860b688a6813024b
tom <salem55.tk@gmail.com>
2018-03-11 08:46:53 +0000

    migrate

--- db/migrate/20180310133317_favorites.rb
+++
@@ -1,8 +0,0 @@
-class Favorites < ActiveRecord::Migration[5.1]
-  def change
-    create_table :favorites do |t|
-      t.integer :user_id
-      t.integer :picture_id
-    end
-  end
-end
--- db/migrate/20180311084404_add_picture_to_users.rb
+++ db/migrate/20180311084404_add_picture_to_users.rb
@@ -1,5 +1,5 @@
 class AddPictureToUsers < ActiveRecord::Migration[5.1]
   def change
-        add_reference :users, :blog, foreign_key: true
+            add_reference :pictures, :user, foreign_key: true
   end
 end
